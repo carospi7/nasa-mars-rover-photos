a21e00f5b54803cf7319578d93d2458f4251f452
carospi7 <carolina.spinetta@gmail.com>
2022-10-13 17:10:30 -0300

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,7 +3,7 @@ import { FooterContainer } from './Footer.styles';
 export default function Footer() {
 	return (
 		<FooterContainer>
-        	<a href="#" target={'_blank'}>Proyect in GitHub</a> | Carolina Spinetta | 2022
+        	<a href="https://github.com/carospi7/nasa-mars-rover-photos" target={'_blank'}>Proyect in GitHub</a> | Carolina Spinetta | 2022
 		</FooterContainer>
 	);
 }
